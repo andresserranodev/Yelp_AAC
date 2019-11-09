7da3a6413b7713d20cd1def69c363a7a4d544539
andres.serrano <andres.serrano@gmail.com>
2019-11-08 20:27:50 -0500

    first commit

---
+++ app/src/main/java/com/puzzlebench/yelp_aac/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.puzzlebench.yelp_aac
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
