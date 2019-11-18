f92661a1e0597c4f24170c0562fde76eaeb2499b
andres.serrano <andres.serrano@gmail.com>
2019-11-18 15:52:31 -0500

    add test fort the view modesl
    Signed-off-by: andres.serrano <andres.serrano@gmail.com>

---
+++ app/src/test/java/com/puzzlebench/yelp_aac/presentation/viewmodel/DetailsBusinessViewModelTest.kt
@@ -0,0 +1,46 @@
+package com.puzzlebench.yelp_aac.presentation.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.puzzlebench.yelp_aac.DummyBusinessDetailsFactory.getBusinessDetailsNoError
+import com.puzzlebench.yelp_aac.repository.BusinessDetailsRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+
+class DetailsBusinessViewModelTest {
+
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var coroutinesTestRule: CoroutinesTestRule = CoroutinesTestRule()
+
+    private val businessId = "1"
+
+    private lateinit var detailsBusinessViewModel: DetailsBusinessViewModel
+    private val businessDetailsRepository = mock<BusinessDetailsRepository> {
+        onBlocking { getBusinessDetailsById(businessId) } doReturn getBusinessDetailsNoError()
+
+    }
+
+    @Before
+    fun setUp() {
+        detailsBusinessViewModel = DetailsBusinessViewModel(businessDetailsRepository, businessId)
+    }
+
+    @Test
+    fun getBusinessDetails() {
+
+        detailsBusinessViewModel.getBusinessDetails()
+        runBlocking {
+            verify(businessDetailsRepository).getBusinessDetailsById(businessId)
+        }
+
+    }
+}
---
+++ app/src/test/java/com/puzzlebench/yelp_aac/presentation/viewmodel/ListBusinessesViewModelTest.kt
@@ -0,0 +1,46 @@
+package com.puzzlebench.yelp_aac.presentation.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.puzzlebench.yelp_aac.DummyBusinessDetailsFactory
+import com.puzzlebench.yelp_aac.DummyBusinessFactory.getBussinesStateNoError
+import com.puzzlebench.yelp_aac.repository.BusinessRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.rules.TestRule
+
+class ListBusinessesViewModelTest {
+
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var coroutinesTestRule: CoroutinesTestRule = CoroutinesTestRule()
+    private lateinit var listBusinessesViewModel: ListBusinessesViewModel
+
+
+    private val businessRepository = mock<BusinessRepository> {
+        onBlocking { getBusiness() } doReturn getBussinesStateNoError()
+
+    }
+
+    @Before
+    fun setUp() {
+        listBusinessesViewModel = ListBusinessesViewModel(businessRepository)
+    }
+
+    @Test
+    fun getBusiness() {
+        listBusinessesViewModel.getBusiness()
+        runBlocking {
+            verify(businessRepository).getBusiness()
+        }
+    }
+}
