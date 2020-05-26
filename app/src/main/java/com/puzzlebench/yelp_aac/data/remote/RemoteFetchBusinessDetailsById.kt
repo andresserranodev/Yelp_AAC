8f1ada2a7f01b30f6144665f8fa0767de25a9d42
andres.serrano <andres.serrano@globant.com>
2020-05-26 14:53:36 -0500

    add code check style ktlint
    https://ktlint.github.io/
    
    and git Git Hook to run the ktlint task durring a commit

--- app/src/main/java/com/puzzlebench/yelp_aac/data/remote/RemoteFetchBusinessDetailsById.kt
+++ app/src/main/java/com/puzzlebench/yelp_aac/data/remote/RemoteFetchBusinessDetailsById.kt
@@ -2,6 +2,6 @@ package com.puzzlebench.yelp_aac.data.remote
 
 import com.puzzlebench.yelp_aac.repository.model.BusinessDetailsState
 
-interface RemoteFetchBusinessDetailsById{
-    suspend fun  fetchBusinessDetailsById(businessId: String): BusinessDetailsState
+interface RemoteFetchBusinessDetailsById {
+    suspend fun fetchBusinessDetailsById(businessId: String): BusinessDetailsState
 }
