dc4d2da92ae1603a4b650cf5f2df81f9a9d3e858
andres.serrano <andres.serrano@globant.com>
2020-05-26 14:16:18 -0500

    test git hook

--- app/src/main/java/com/puzzlebench/yelp_aac/data/remote/RemoteFetchBusinessDetailsById.kt
+++ app/src/main/java/com/puzzlebench/yelp_aac/data/remote/RemoteFetchBusinessDetailsById.kt
@@ -3,5 +3,5 @@ package com.puzzlebench.yelp_aac.data.remote
 import com.puzzlebench.yelp_aac.repository.model.BusinessDetailsState
 
 interface RemoteFetchBusinessDetailsById{
-    suspend fun fetchBusinessDetailsById(businessId: String): BusinessDetailsState
+    suspend fun  fetchBusinessDetailsById(businessId: String): BusinessDetailsState
 }
