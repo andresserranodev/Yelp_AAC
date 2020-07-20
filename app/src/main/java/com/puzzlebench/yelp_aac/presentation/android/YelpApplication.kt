92d7f58fb0ea783e148ba0366021e2058190606a
adsf117 <@Thecats.05>
2020-07-20 15:41:43 -0500

    feature  sync the data daily  *Schedule repeating alarm  *call service and Delete all Local Data

--- app/src/main/java/com/puzzlebench/yelp_aac/presentation/android/YelpApplication.kt
+++ app/src/main/java/com/puzzlebench/yelp_aac/presentation/android/YelpApplication.kt
@@ -23,7 +23,7 @@ class YelpApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG){
+        if (BuildConfig.DEBUG) {
             Stetho.initializeWithDefaults(this)
         }
         initSyncAlarm()
